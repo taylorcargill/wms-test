4e43a25bdb55fdbb3fd6ae487675a76047acc79d
Taylor Cargill <taylor.cargill@tcgplayer.com>
2025-06-13 14:00:19 -0400

    test

---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,34 @@
+import { AppShell, Burger, Group, Skeleton, AppShellNavbar } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+
+export function HomePage() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: { base: 60, md: 70, lg: 80 } }}
+      navbar={{
+        width: { base: 200, md: 300, lg: 400 },
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShellNavbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShellNavbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
+}
